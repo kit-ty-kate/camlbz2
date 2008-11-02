fcfc5646679c84d8360de03508f6a0863e2f5ea9
Stefano Zacchiroli <zack@upsilon.cc>
2008-11-03 00:02:37 +0100

    refresh io.h wrt latest OCaml

--- io.h
+++ io.h
@@ -1,4 +1,17 @@
-/* Taken from byterun/io.h in the OCAML v3.00 source code (©1996 INRIA) */
+/***********************************************************************/
+/*                                                                     */
+/*                           Objective Caml                            */
+/*                                                                     */
+/*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         */
+/*                                                                     */
+/*  Copyright 1996 Institut National de Recherche en Informatique et   */
+/*  en Automatique.  All rights reserved.  This file is distributed    */
+/*  under the terms of the GNU Library General Public License, with    */
+/*  the special exception on linking described in file LICENSE.        */
+/*                                                                     */
+/***********************************************************************/
+
+/* Snippet taken from byterun/io.h of OCaml 3.11 */
 
 #include <caml/mlvalues.h>
 
@@ -6,13 +19,21 @@
 #define IO_BUFFER_SIZE 4096
 #endif
 
+#include <sys/types.h>
+typedef off_t file_offset;
+
 struct channel {
   int fd;                       /* Unix file descriptor */
-  long offset;                  /* Absolute position of fd in the file */
+  file_offset offset;           /* Absolute position of fd in the file */
   char * end;                   /* Physical end of the buffer */
   char * curr;                  /* Current position in the buffer */
   char * max;                   /* Logical end of the buffer (for input) */
   void * mutex;                 /* Placeholder for mutex (for systhreads) */
+  struct channel * next, * prev;/* Double chaining of channels (flush_all) */
+  int revealed;                 /* For Cash only */
+  int old_revealed;             /* For Cash only */
+  int refcount;                 /* For flush_all and for Cash */
+  int flags;                    /* Bitfield */
   char buff[IO_BUFFER_SIZE];    /* The buffer itself */
 };
 
